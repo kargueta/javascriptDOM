fbcd91eeb96f9a69bcbff67d4c206a1ddac00333
Kevin Argueta <kevinargueta@Kevins-MacBook-Pro.local>
2018-08-09 21:22:00 -0400

    lesson 6: properties of of Nodes(nodeType, nodeName, hasChildNodes) and cloning nodes

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
+const banner = document.querySelector('#page-banner');
 
-// This method querySelectorAll returns all the hits
-// unlike querySelector which only returns the first hit
-var books = document.querySelectorAll('#book li .name');
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner node has child nodes?:', banner.hasChildNodes());
 
-
-Array.from(books).forEach(function(book){
-	console.log(book);
-});
+const clonedBanner = banner.cloneNode(true);
